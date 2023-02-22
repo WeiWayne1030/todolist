5349be6173317e984e2c4818c270faf3c40dca53
WeiWayne1030 <wayne10032004@gmail.com>
2023-02-23 01:09:11 +0800

    refactor: modify app.js

--- app.js
+++ app.js
@@ -36,52 +36,7 @@ app.use(methodOverride('_method'))
 
 app.use(routes)
 
-app.get('/todos/new', (req, res) =>{
-  return res.render('new')
-})
 
-app.post('/todos', (req,res) =>{
-  const name = req.body.name
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id', (req, res) =>{
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('detail', {todo}))
-    .catch(error => console.log(error))
-})
-app.get('/todos/:id/edit', (req, res) =>{
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('edit', {todo}))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) =>{
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo =>{
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(()=> res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req,res) =>{
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 app.listen(3000, () =>{
   console.log('App is running on http://localhost:3000')
