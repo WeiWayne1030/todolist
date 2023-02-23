c138d8270b8924709cbcba9f5d657ab6b7252948
WeiWayne1030 <wayne10032004@gmail.com>
2023-02-23 11:20:21 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,39 +1,17 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
-const Todo = require('./models/todo')
-const { findById } = require('./models/todo')
 
 const routes = require('./routes') //預設就會去找index
+const app = express()
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-// 設定連線到 mongoDB
-mongoose.connect('mongodb+srv://Wayne:weiSun1030@cluster0.pflem8h.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout:'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
-
 app.use(routes)
 
 
