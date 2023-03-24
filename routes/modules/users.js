d76323e1f59a828df6cd9d4b8d69ecf33431b2d4
WeiWayne1030 <wayne10032004@gmail.com>
2023-03-24 14:07:17 +0800

    feat: add user model

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
+const User = require('../../models/user')
 
 router.get('/login', (req, res) =>{
   res.render('login')
@@ -9,8 +10,33 @@ router.get('/login', (req, res) =>{
 
 router.post('/login', (req, res) => {
 })
+
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const {name, email, password, confirmPassword} = req.body
+  User.findOne({email}).then(user => {
+    if (user) {
+      console.log('User aleady exits.')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+  })
+  .catch(err => console.log(err))
 })
 
 module.exports = router
